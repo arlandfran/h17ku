efe20594e005e3b4615758856a2b4a12d45c2db9
Arland <arland.torres@outlook.com>
2021-11-03 20:47:18 +0000

    refactor responsive implementation

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -37,41 +37,33 @@
     </svg>
   </button>
 
-  {#if $media.mobile}
-    <!-- Hamburger -->
-    <button
-      class="p-2"
-      class:hidden={$media.desktop}
-      on:click={menuClickHandler}
+  <!-- Hamburger -->
+  <button class="p-2 md:hidden" on:click={menuClickHandler}>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="w-6 h-6"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="w-6 h-6"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M4 6h16M4 12h16M4 18h16"
-        />
-      </svg>
-    </button>
-  {/if}
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M4 6h16M4 12h16M4 18h16"
+      />
+    </svg>
+  </button>
 
-  {#if $media.desktop}
-    <nav>
-      <ul class="flex gap-2">
-        {#each navLinks as { link, href }}
-          <li>
-            <a class="p-2" {href}>{link}</a>
-          </li>
-        {/each}
-      </ul>
-    </nav>
-  {/if}
+  <nav class="hidden md:block">
+    <ul class="flex gap-2">
+      {#each navLinks as { link, href }}
+        <li>
+          <a class="p-2" {href}>{link}</a>
+        </li>
+      {/each}
+    </ul>
+  </nav>
 </header>
 
 {#if showMenu}
