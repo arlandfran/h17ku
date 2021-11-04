a003b5bbee75417dcf7716af72c29efadb161cd2
Arland <arland.torres@outlook.com>
2021-11-04 16:06:57 +0000

    extract form label and input into component

--- src/pages/register/index.svelte
+++ src/pages/register/index.svelte
@@ -1,45 +1,30 @@
+<script>
+  import FormInput from "../../components/FormInput.svelte";
+</script>
+
 <h1 class="my-4 text-3xl font-bold dark:text-white">create your account</h1>
 
 <div class="w-full max-w-2xl min-w-xs dark:text-white">
   <form class="py-6">
-    <div class="mb-4">
-      <label class="block mb-2 font-bold" for="email"> email </label>
-      <input
-        class="px-3 py-2 w-full leading-tight rounded shadow-lg appearance-none focus:outline-none focus:ring-black dark:ring-white focus:ring-2 dark:bg-gray-700"
-        id="email"
-        type="email"
-        placeholder="email"
-      />
-    </div>
-    <div class="mb-4">
-      <label class="block mb-2 font-bold" for="username"> username </label>
-      <input
-        class="px-3 py-2 w-full leading-tight rounded shadow-lg appearance-none focus:outline-none focus:ring-black dark:ring-white focus:ring-2 dark:bg-gray-700"
-        id="username"
-        type="text"
-        placeholder="username"
-      />
-    </div>
-    <div class="mb-4">
-      <label class="block mb-2 font-bold" for="password"> password </label>
-      <input
-        class="px-3 py-2 w-full leading-tight rounded shadow-lg appearance-none focus:outline-none focus:ring-black dark:ring-white focus:ring-2 dark:bg-gray-700"
-        id="password"
-        type="password"
-        placeholder="***********"
-      />
-    </div>
-    <div class="mb-4">
-      <label class="block mb-2 font-bold" for="password">
-        confirm password
-      </label>
-      <input
-        class="px-3 py-2 w-full leading-tight rounded shadow-lg appearance-none focus:outline-none focus:ring-black dark:ring-white focus:ring-2 dark:bg-gray-700"
-        id="password"
-        type="password"
-        placeholder="***********"
-      />
-    </div>
+    <FormInput name="email" label="email" type="email" placeholder="email" />
+    <FormInput
+      name="user"
+      label="username"
+      type="text"
+      placeholder="username"
+    />
+    <FormInput
+      name="password1"
+      label="password"
+      type="password"
+      placeholder="***********"
+    />
+    <FormInput
+      name="password2"
+      label="confirm password"
+      type="password"
+      placeholder="***********"
+    />
     <div class="flex justify-center">
       <button
         class="px-4 py-2 text-2xl font-bold dark:text-white focus:outline-none focus:ring-black dark:ring-white focus:ring-2"
