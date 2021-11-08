60c9c1fd05243d24ba26d53338b3a7f63061d3c9
Arland <arland.torres@outlook.com>
2021-11-08 23:18:39 +0000

    implement login form ui

--- src/pages/login/index.svelte
+++ src/pages/login/index.svelte
@@ -1,12 +1,28 @@
 <script>
   import { params } from "@roxi/routify";
   import { onMount } from "svelte";
+  import { createForm } from "svelte-forms-lib";
   import { SvelteToast as Toast, toast } from "@zerodevx/svelte-toast";
+  import * as yup from "yup";
 
   onMount(() => {
     if ($params.newUser) {
       toast.push("new account created!", options);
     }
+  });
+
+  const { form, errors, handleChange, handleSubmit } = createForm({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: yup.object().shape({
+      email: yup.string().email().required(),
+      password: yup.string().required(),
+    }),
+    onSubmit: async (values) => {
+      console.log(JSON.stringify(values));
+    },
   });
 
   const options = {
@@ -24,6 +40,52 @@
 
 <h1 class="text-4xl font-bold dark:text-white">log in</h1>
 
+<div class="w-full max-w-2xl min-w-xs dark:text-white">
+  <form on:submit={handleSubmit} class="py-6">
+    <div class="mb-4">
+      <label class="block mb-2 font-bold" for="email"> email</label>
+      <input
+        class:error={$errors.email}
+        class="px-3 py-2 w-full leading-tight rounded shadow-lg appearance-none focus:outline-none focus:ring-black dark:ring-white focus:ring-2 dark:bg-gray-700"
+        id="email"
+        type="email"
+        placeholder="email address"
+        on:change={handleChange}
+        bind:value={$form.email}
+        on:invalid|preventDefault
+      />
+      {#if $errors.email}
+        <small class="py-2 text-xs text-red-600">{$errors.email}</small>
+      {/if}
+    </div>
+
+    <div class="mb-4">
+      <label class="block mb-2 font-bold" for="email"> password </label>
+      <input
+        class:error={$errors.password}
+        class="px-3 py-2 w-full leading-tight rounded shadow-lg appearance-none focus:outline-none focus:ring-black dark:ring-white focus:ring-2 dark:bg-gray-700"
+        id="password"
+        type="password"
+        placeholder="**********"
+        on:change={handleChange}
+        bind:value={$form.password}
+      />
+      {#if $errors.password}
+        <small class="py-2 text-xs text-red-600">{$errors.password}</small>
+      {/if}
+    </div>
+
+    <div class="flex justify-center">
+      <button
+        class="px-4 py-2 text-xl font-bold dark:text-white focus:outline-none focus:ring-black dark:ring-white focus:ring-2 disabled:line-through disabled:cursor-default"
+        type="submit"
+      >
+        log in
+      </button>
+    </div>
+  </form>
+</div>
+
 <Toast />
 
 <style>
@@ -32,5 +94,9 @@
     --toastContainerRight: auto;
     --toastContainerBottom: 1rem;
     --toastContainerLeft: calc(50vw - 8rem);
+  }
+
+  .error {
+    @apply border border-red-600 ring-red-600;
   }
 </style>
