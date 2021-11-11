2b8588224d189732abe6b7cb8c390b1017654b6b
Arland <arland.torres@outlook.com>
2021-11-11 22:50:53 +0000

    only show logout if authenticated

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -3,16 +3,29 @@
   import Hamburger from "../components/Hamburger.svelte";
   import { fade, slide } from "svelte/transition";
   import { media } from "svelte-match-media";
+  import { isAuthenticated } from "../stores";
 
   let showMenu = false;
 
-  const navLinks = [
+  const defaultLinks = [
     { link: "log in", href: "./login" },
     { link: "register", href: "./register" },
   ];
 
   $: if ($media.desktop) {
     showMenu = false;
+  }
+
+  function logout() {
+    fetch("/api/auth/logout", {
+      credentials: "same-origin",
+    })
+      .then(() => {
+        $isAuthenticated = false;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 </script>
 
@@ -25,16 +38,25 @@
     <Hamburger bind:showMenu />
 
     <nav class="hidden md:block">
-      <ul class="flex gap-2">
-        {#each navLinks as { link, href }}
-          <li>
-            <a
-              class="p-2 rounded-sm focus:outline-none focus:ring-2 focus:ring-black dark:ring-white"
-              {href}>{link}</a
-            >
-          </li>
-        {/each}
-      </ul>
+      {#if $isAuthenticated}
+        <ul class="flex gap-2">
+          <button
+            class="p-2 rounded-sm focus:outline-none focus:ring-2 focus:ring-black dark:ring-white"
+            on:click={logout}>logout</button
+          >
+        </ul>
+      {:else}
+        <ul class="flex gap-2">
+          {#each defaultLinks as { link, href }}
+            <li>
+              <a
+                class="p-2 rounded-sm focus:outline-none focus:ring-2 focus:ring-black dark:ring-white"
+                {href}>{link}</a
+              >
+            </li>
+          {/each}
+        </ul>
+      {/if}
     </nav>
   </div>
 
@@ -43,16 +65,25 @@
       class="flex justify-end pt-4 w-full max-w-7xl text-right"
       transition:slide={{ duration: 200 }}
     >
-      <ul class="flex flex-col gap-4" in:fade>
-        {#each navLinks as { link, href }}
-          <li>
-            <a
-              class="p-2 rounded-sm focus:outline-none focus:ring-2 focus:ring-black dark:ring-white"
-              {href}>{link}</a
-            >
-          </li>
-        {/each}
-      </ul>
+      {#if $isAuthenticated}
+        <ul class="flex flex-col gap-4" in:fade>
+          <button
+            class="p-2 rounded-sm focus:outline-none focus:ring-2 focus:ring-black dark:ring-white"
+            on:click={logout}>logout</button
+          >
+        </ul>
+      {:else}
+        <ul class="flex flex-col gap-4" in:fade>
+          {#each defaultLinks as { link, href }}
+            <li>
+              <a
+                class="p-2 rounded-sm focus:outline-none focus:ring-2 focus:ring-black dark:ring-white"
+                {href}>{link}</a
+              >
+            </li>
+          {/each}
+        </ul>
+      {/if}
     </nav>
   {/if}
 </header>
