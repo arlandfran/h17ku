113694f73e2f4e1ea4a1835bbfda913b34761b19
Arland <arland.torres@outlook.com>
2021-11-12 01:13:06 +0000

    check session on refresh or page navigation

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,27 +1,7 @@
 <script>
-  import { onMount } from "svelte";
   import { isAuthenticated } from "../stores";
-
-  let id;
-
-  onMount(() => {
-    fetch("/api/auth/session", {
-      credentials: "same-origin",
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.login) {
-          $isAuthenticated = true;
-          id = data.id;
-        }
-      });
-  });
 </script>
 
 <h1 class="text-4xl font-bold dark:text-white">haiku*</h1>
 
 <p class="dark:text-white">authenticated: {$isAuthenticated}</p>
-
-{#if $isAuthenticated}
-  <p class="dark:text-white">id: {id}</p>
-{/if}
