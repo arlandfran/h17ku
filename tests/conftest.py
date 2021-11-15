20fbca0182e4e59d6200cf3c078bdb3544aacb62
Arland <arland.torres@outlook.com>
2021-11-15 01:14:21 +0000

    refactor test user objects into pytest fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,15 @@
 import pytest
 from flask_pymongo import PyMongo
-from flask_login import LoginManager
+from faker import Faker
+from faker.providers import internet, misc
 
 from app import create_app
 from app.models import User
+
+
+fake = Faker()
+fake.add_provider(internet)
+fake.add_provider(misc)
 
 
 @pytest.fixture
@@ -25,4 +31,38 @@ def mongo():
 @pytest.fixture(scope="module")
 def new_user():
     user = User("test@test.com", "test_user")
+    return user
+
+
+@pytest.fixture(scope="module")
+def new_fake_user():
+    same_password = fake.password(length=8)
+    user = {
+        "email": fake.ascii_safe_email(),
+        "username": fake.user_name(),
+        "password": same_password,
+        "password2": same_password,
+    }
+    return user
+
+
+@pytest.fixture(scope="function")
+def email_exists(new_fake_user):
+    user = {
+        "email": new_fake_user["email"],
+        "username": fake.user_name(),
+        "password": new_fake_user["password"],
+        "password2": new_fake_user["password"],
+    }
+    return user
+
+
+@pytest.fixture(scope="function")
+def username_exists(new_fake_user):
+    user = {
+        "email": fake.ascii_safe_email(),
+        "username": new_fake_user["username"],
+        "password": new_fake_user["password"],
+        "password2": new_fake_user["password"],
+    }
     return user
