f2522b743a37870b5469034b305cbc58fe1ba091
Arland <arland.torres@outlook.com>
2021-11-16 13:53:10 +0000

    use environ.get() instead of .getenv() for heroku

--- config.py
+++ config.py
@@ -9,20 +9,20 @@ class Config:
 
     DEBUG = False
     TESTING = False
-    SECRET_KEY = os.getenv("SECRET_KEY")
+    SECRET_KEY = os.environ.get("SECRET_KEY")
     SESSION_COOKIE_HTTPONLY = True
     REMEMBER_COOKIE_HTTPONLY = True
     SESSION_COOKIE_SAMESITE = "Strict"
 
 
 class DevConfig(Config):
-    MONGO_URI = os.getenv("MONGO_DEV_URI")
+    MONGO_URI = os.environ.get("MONGO_DEV_URI")
 
 
 class TestConfig(Config):
     TESTING = True
     WTF_CSRF_ENABLED = False
-    MONGO_URI = os.getenv("MONGO_TEST_URI")
+    MONGO_URI = os.environ.get("MONGO_TEST_URI")
 
 
 class TestCSRFConfig(TestConfig):
@@ -32,7 +32,7 @@ class TestCSRFConfig(TestConfig):
 class ProdConfig(Config):
     SESSION_COOKIE_SECURE = True
     REMEMBER_COOKIE_SECURE = True
-    MONGO_URI = os.getenv("MONGO_PROD_URI")
+    MONGO_URI = os.environ.get("MONGO_PROD_URI")
 
 
 config = {
