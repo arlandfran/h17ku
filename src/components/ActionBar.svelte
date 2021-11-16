d6560a60b29798ea8409fa68aa2f31da3245bdc0
Arland <arland.torres@outlook.com>
2021-11-16 04:51:49 +0000

    disable post button if not authenticated
    - show tooltip to login
    - uses tippy.js

--- src/components/ActionBar.svelte
+++ src/components/ActionBar.svelte
@@ -1,4 +1,23 @@
 <script>
+  import { onMount } from "svelte";
+  import { isAuthenticated } from "../stores";
+  import tippy from "tippy.js";
+  import "tippy.js/dist/tippy.css";
+  import "tippy.js/themes/translucent.css";
+
+  onMount(() => {
+    if (!$isAuthenticated) {
+      tippy("#post", {
+        content: "you must be logged in to post haikus",
+        arrow: false,
+        hideOnClick: false,
+        trigger: "mouseenter focus",
+        placement: "bottom",
+        theme: "translucent",
+      });
+    }
+  });
+
   async function copy() {
     const text = document.getElementById("haiku-validator");
     await navigator.clipboard.writeText(text.value);
@@ -12,10 +31,21 @@
 </script>
 
 <div class="flex gap-4 justify-end dark:text-white">
-  <button
-    class="p-2 rounded-sm focus:outline-none focus:ring-2 focus:ring-black dark:ring-white"
-    >post</button
-  >
+  {#if $isAuthenticated}
+    <button
+      class="p-2 rounded-sm focus:outline-none focus:ring-2 focus:ring-black dark:ring-white"
+      >post</button
+    >
+  {:else}
+    <span
+      id="post"
+      tabindex="0"
+      class="rounded-sm focus:outline-none focus:ring-2 focus:ring-black dark:ring-white"
+    >
+      <button class="p-2 line-through" disabled>post</button>
+    </span>
+  {/if}
+
   <button
     id="copy"
     class="p-2 rounded-sm focus:outline-none focus:ring-2 focus:ring-black dark:ring-white"
