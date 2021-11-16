ba5382559da3ebed6a5211eb4f53facdded5d125
Arland <arland.torres@outlook.com>
2021-11-16 04:59:45 +0000

    remove auth check and add welcome text

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,10 +1,15 @@
 <script>
   import HaikuValidator from "../components/HaikuValidator.svelte";
-  import { isAuthenticated } from "../stores";
 </script>
 
-<h1 class="text-4xl font-bold dark:text-white">haiku*</h1>
+<section class="mb-4 w-full max-w-2xl">
+  <h1 class="mb-2 text-4xl font-bold text-center dark:text-white">haiku*</h1>
 
-<p class="dark:text-white">authenticated: {$isAuthenticated}</p>
+  <p class="dark:text-white">
+    *a form of japanese poetry - a haiku expresses a single feeling or
+    impression and contains three unrhymed lines of five, seven, and five
+    syllables, respectively.
+  </p>
+</section>
 
 <HaikuValidator />
