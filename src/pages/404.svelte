fd3034134163efdd1d86751a540b143a71db4043
Arland <arland.torres@outlook.com>
2021-11-17 15:40:58 +0000

    adjust view height

--- src/pages/404.svelte
+++ src/pages/404.svelte
@@ -1,13 +1,13 @@
-<div class="flex justify-center items-center w-screen">
-  <section class="text-center">
+<div class="flex justify-center items-center w-screen vh-6">
+  <div class="text-center">
     <h1 class="mb-4 text-3xl font-bold">Page not found.</h1>
 
     <a href="/" class="">Go back</a>
-  </section>
+  </div>
 </div>
 
 <style>
-  div {
-    height: 80vh;
+  .vh-6 {
+    height: 60vh;
   }
 </style>
