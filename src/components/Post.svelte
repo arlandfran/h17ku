b444d81c99815429335d4f3442e08b7764f16710
Arland <arland.torres@outlook.com>
2021-11-18 13:56:56 +0000

    fix elapsed time not updating when post added

--- src/components/Post.svelte
+++ src/components/Post.svelte
@@ -1,24 +1,77 @@
 <script>
   import { getElapsedTime } from "../helpers";
+  import { isAuthenticated, user } from "../stores";
+  import { url } from "@roxi/routify";
+  import CommentForm from "../components/CommentForm.svelte";
+  import { onMount, afterUpdate } from "svelte";
 
+  export let _id;
   export let author;
   export let haiku;
   export let likes;
   export let created_at;
+  export let isSelected = false;
+  export let comments = [];
 
-  const time = new Date(created_at.$date);
-  const elapsedTime = getElapsedTime(time);
+  let time = new Date(created_at.$date);
+  let isEditing = false;
+  let elapsedTime;
+
+  onMount(() => {
+    elapsedTime = getElapsedTime(time);
+  });
+
+  afterUpdate(() => {
+    time = new Date(created_at.$date);
+    elapsedTime = getElapsedTime(time);
+  });
+
+  const editHandler = () => (isEditing = !isEditing);
 </script>
 
-<div class="px-4 py-4 w-full border-b border-black dark:border-gray-400">
-  <div class="mb-4 font-mono whitespace-pre-line">
-    {haiku}
+<div
+  class="flex flex-col gap-y-4 px-4 py-4 w-full border-b border-black dark:border-gray-400"
+>
+  <div>
+    <span
+      >{author} •
+      <time datetime={time.toISOString()}>{elapsedTime}</time></span
+    >
   </div>
 
-  <div class="flex gap-4">
-    <span
-      >{author} • <time datetime={time.toISOString()}>{elapsedTime}</time></span
-    >
+  {#if isEditing}
+    textarea
+  {:else}
+    <div class="font-mono whitespace-pre-line">
+      {haiku}
+    </div>
+  {/if}
+
+  <div class="flex gap-4 items-center">
     <span>likes: {likes}</span>
+    {#if $user !== author}
+      <button class="btn"> save </button>
+    {/if}
+
+    {#if !isSelected}
+      <a href={$url("/:user/:id", { user: author, id: _id.$oid })} class="link">
+        comments
+      </a>
+    {/if}
+
+    {#if $isAuthenticated && $user === author}
+      <button class="btn" type="button" on:click={editHandler}>edit</button>
+      <button class="btn">delete</button>
+    {/if}
   </div>
+
+  {#if isSelected}
+    {#each comments as comment}
+      {comment}
+    {/each}
+  {/if}
 </div>
+
+{#if isSelected}
+  <CommentForm {author} />
+{/if}
