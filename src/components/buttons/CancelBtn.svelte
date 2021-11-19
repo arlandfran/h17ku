2dbe92e3463e11bc2094842cd48da36dc270c0a4
Arland <arland.torres@outlook.com>
2021-11-19 01:09:14 +0000

    refactor buttons into components

---
+++ src/components/buttons/CancelBtn.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let state;
+</script>
+
+<button class="transition btn hover:bg-red-500" on:click={() => (state = false)}
+  ><svg
+    xmlns="http://www.w3.org/2000/svg"
+    class="w-6 h-6"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M6 18L18 6M6 6l12 12"
+    />
+  </svg></button
+>
