f71c4d4fd3c2a221c68ca9bd083ae68eeb98d18b
Arland <arland.torres@outlook.com>
2021-11-20 03:15:45 +0000

    pass in form id to submit edit button

--- src/components/buttons/SubmitEdit.svelte
+++ src/components/buttons/SubmitEdit.svelte
@@ -1,7 +1,11 @@
+<script>
+  export let form;
+</script>
+
 <button
   class="transition btn hover:bg-green-500 focus:ring-green-500 focus:bg-green-500 hover:text-white focus:text-white"
   type="submit"
-  form="edit-form"
+  {form}
   ><svg
     xmlns="http://www.w3.org/2000/svg"
     class="w-6 h-6"
