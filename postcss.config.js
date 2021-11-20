77322662b0c6a53dbb3d31bc8b2343f1252120ec
Arland <arland.torres@outlook.com>
2021-11-20 05:39:34 +0000

    put tailwind first in postcss plugin list

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   plugins: [
-    require("postcss-import"),
     require("tailwindcss"),
     require("autoprefixer")({
       overrideBrowserslist: ["defaults and last 4 versions"],
     }),
+    require("postcss-import"),
   ],
 };
